f4e243c2d17ebe96fee73becd6f91b2b1d1bc433
cavolarl <carl.lavos@gmail.com>
2025-06-25 13:01:08 +0200

    card assets added

--- src/game/main.ts
+++ src/game/main.ts
@@ -1,26 +1,22 @@
-import { Boot } from './scenes/Boot';
-import { GameOver } from './scenes/GameOver';
 import { Game as MainGame } from './scenes/Game';
-import { MainMenu } from './scenes/MainMenu';
 import { AUTO, Game } from 'phaser';
-import { Preloader } from './scenes/Preloader';
 
 // Find out more information about the Game Config at:
 // https://docs.phaser.io/api-documentation/typedef/types-core#gameconfig
 const config: Phaser.Types.Core.GameConfig = {
     type: AUTO,
-    width: 1024,
-    height: 768,
     parent: 'game-container',
     backgroundColor: '#028af8',
-    scene: [
-        Boot,
-        Preloader,
-        MainMenu,
-        MainGame,
-        GameOver
-    ]
+    scene: [MainGame],
+    scale: {
+        mode: Phaser.Scale.RESIZE,
+        width: '100%',
+        height: '100%'
+    }
 };
+
+
+
 
 const StartGame = (parent: string) => {
 
--- src/game/scenes/Game.ts
+++ src/game/scenes/Game.ts
@@ -3,34 +3,56 @@ import { Scene } from 'phaser';
 
 export class Game extends Scene
 {
-    camera: Phaser.Cameras.Scene2D.Camera;
-    background: Phaser.GameObjects.Image;
-    gameText: Phaser.GameObjects.Text;
-
     constructor ()
     {
         super('Game');
     }
 
+    preload() {
+    this.load.image('clubs', 'assets/card-clubs-1.png');
+    this.load.image('diamonds', 'assets/card-diamonds-1.png');
+    this.load.image('hearts', 'assets/card-hearts-1.png');
+    this.load.image('spades', 'assets/card-spades-1.png');
+    this.load.image('back', 'assets/card-back1.png');
+    }
+
+
     create ()
-    {
-        this.camera = this.cameras.main;
-        this.camera.setBackgroundColor(0x00ff00);
+{
+    this.cameras.main.setBackgroundColor('#6aa84f');
 
-        this.background = this.add.image(512, 384, 'background');
-        this.background.setAlpha(0.5);
+    const cardWidth = 100;
+    const cardHeight = 140;
+    const cardSpacing = 20;
 
-        this.gameText = this.add.text(512, 384, 'Make something fun!\nand share it with us:\nsupport@phaser.io', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        }).setOrigin(0.5).setDepth(100);
+    const cardPositions = [
+        { x: 100, y: 100, key: 'clubs' },
+        { x: 100 + cardWidth + cardSpacing, y: 100, key: 'diamonds' },
+        { x: 100 + 2 * (cardWidth + cardSpacing), y: 100, key: 'hearts' },
+        { x: 100 + 3 * (cardWidth + cardSpacing), y: 100, key: 'spades' }
+    ];
 
-        EventBus.emit('current-scene-ready', this);
-    }
+    cardPositions.forEach(pos => {
+        // Start with the card back
+        const cardBack = this.add.image(pos.x, pos.y, 'back')
+            .setOrigin(0.5)
+            .setDisplaySize(cardWidth, cardHeight)
+            .setInteractive();
 
-    changeScene ()
-    {
-        this.scene.start('GameOver');
-    }
+        // Create the face but make it invisible initially
+        const cardFace = this.add.image(pos.x, pos.y, pos.key)
+            .setOrigin(0.5)
+            .setDisplaySize(cardWidth, cardHeight)
+            .setVisible(false);
+
+        // On click, hide the back and show the face
+        cardBack.on('pointerdown', () => {
+            cardBack.setVisible(false);
+            cardFace.setVisible(true);
+        });
+    });
+
+    EventBus.emit('current-scene-ready', this);
+}
+
 }
